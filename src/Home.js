5a25af994639420b4e5b2ec11b32deee94255533
Guilherme Marcelino da Silva <34476524+gmspgua@users.noreply.github.com>
2020-08-23 11:39:49 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
         return (
           <div>
               <div className="header">
-                <h1>Bem Vindo ao mundo Devops!</h1>
+                <h1>Copadubo - Paranaguá</h1>
               </div>
               <div className="content" id="content">
               </div>
