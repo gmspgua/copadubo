14ef8f87ab6acce61bc43edec71670cd9e0547b7
Guilherme Marcelino da Silva <34476524+gmspgua@users.noreply.github.com>
2020-08-22 23:52:29 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
         return (
           <div>
               <div className="header">
-                <h1>Horizon, Bem vindo ao mundo DEvops!</h1>
+                <h1>Horizon, Bem vindo ao mundo Devops!</h1>
               </div>
               <div className="content" id="content">
               </div>
